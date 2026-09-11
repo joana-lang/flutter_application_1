2dcd4372e975be46325b23ffa35dbea42692fd38
Sinai-chan <chanjoana03@gmail.com>
2026-09-10 19:26:18 -0600

    feat(login): add email and password inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,6 +9,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _obscure = true; //control para mostrar o ocultar contraseña
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
@@ -21,7 +22,43 @@ class _LoginScreenState extends State<LoginScreen> {
               SizedBox(
                 width: size.width,
                 height: 200,
-                child: RiveAnimation.asset('login-bear.riv'),
+                child: const RiveAnimation.asset('login-bear.riv'),
+              ),
+              //para separar
+              const SizedBox(height: 10),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                    hintText: 'Email',
+                    prefixIcon: const Icon(Icons.email),
+                    border: OutlineInputBorder(
+                        //para redondear
+                        borderRadius: BorderRadius.circular(12))),
+              ),
+
+              const SizedBox(height: 10), //para salto de linea
+              //Campo de texto para contraseña
+              TextField(
+                obscureText: _obscure,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Contraseña',
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        //refrescar iconos
+                        setState(() {
+                          _obscure = !_obscure;
+                        });
+                      },
+                      icon: Icon(
+                        _obscure ? Icons.visibility : Icons.visibility_off,
+                      )),
+                  border: OutlineInputBorder(
+                    //para redondear
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
               ),
             ],
           ),
